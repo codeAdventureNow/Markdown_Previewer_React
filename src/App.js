7571b45b2f91a8cbbc3e2dd341b9a15a42405470
[David <[nmueller855@gmail.com]>
2023-03-09 15:57:22 -0800

    I have text coming on the previewer in html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { marked } from 'marked';
+import { useState } from 'react';
 // import { JSDOM } from 'jsdom';
 // import DOMPurify from 'dompurify';
 
@@ -8,8 +9,30 @@ import { marked } from 'marked';
 
 function App() {
   // const clean = purify.sanitize('<b>hello there</b>');
-  const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
-  return <div className='App'>{html}</div>;
+  const [preview, setPreview] = useState('');
+  console.log(preview);
+  const html = marked.parse(preview);
+  // const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
+
+  return (
+    <div className='App'>
+      <div id='editor'>
+        <h3 className='title'>Editor</h3>
+
+        <textarea
+          className='text-area'
+          value={preview}
+          onChange={(e) => setPreview(e.target.value)}
+        ></textarea>
+      </div>
+
+      <div id='preview'>
+        <h3 className='title'>Previewer</h3>
+
+        <p>{html}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
